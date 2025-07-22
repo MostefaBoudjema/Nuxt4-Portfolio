cdcd084f273883034c5471ad403de957f73ddb9c
mostfa <myreal17@gmail.com>
2025-07-22 10:04:51 +0100

    update blog list lang

--- components/blog/BlogList.vue
+++ components/blog/BlogList.vue
@@ -1,8 +1,10 @@
 <script setup>
 import BlogPost from './BlogPost.vue';
 import posts from '../../data/posts.js';
+import { useI18n } from 'vue-i18n';
 
-const filteredPosts = posts.filter(post => post.published).map(post => ({
+const { locale }=useI18n();
+const filteredPosts=posts.filter(post => post.lang===locale.value).map(post => ({
   ...post,
   image: `https://picsum.photos/600/300?random=${post.id}`
 }));
@@ -12,15 +14,19 @@ const filteredPosts = posts.filter(post => post.published).map(post => ({
   <div class="container mx-auto">
     <div class="blog-list font-general-regular text-ternary-dark dark:text-ternary-light">
       <div class="text-center">
-      <p class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light">
-        {{$t('blog.latestArticle')}}
-      </p>
-    </div>
-      <BlogPost
-        v-for="post in filteredPosts"
-        :key="post.id"
-        :post="post"
-      />
+        <p
+          class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light">
+          {{ $t('blog.latestArticle') }}
+        </p>
+      </div>
+      <div v-if="filteredPosts.length > 0">
+        <BlogPost v-for="post in filteredPosts" :key="post.id" :post="post" />
+      </div>
+      <div v-else class="text-center mt-10">
+        <p class="text-lg text-gray-500 dark:text-gray-400">
+          {{ $t('blog.noPosts') }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -31,4 +37,4 @@ const filteredPosts = posts.filter(post => post.published).map(post => ({
   flex-direction: column;
   gap: 1.5rem;
 }
-</style> 
+</style>
