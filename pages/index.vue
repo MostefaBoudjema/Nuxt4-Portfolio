08e1189bc56802725667e4531aca105b73285157
mostfa <myreal17@gmail.com>
2025-08-05 23:27:12 +0100

    add medicio

--- pages/index.vue
+++ pages/index.vue
@@ -6,16 +6,14 @@
 
     <!-- Projects -->
     <div class="sm:container sm:mx-auto">
-      <ProjectsGrid data-aos="fade-up" data-aos-delay="200"/>
+      <ProjectsGrid data-aos="fade-up" data-aos-delay="200" />
 
       <!-- Load more projects button -->
       <div class="mt-10 sm:mt-20 flex justify-center">
-        <NuxtLink
-          :to="localePath('/projects')"
+        <NuxtLink :to="localePath('/projects')"
           class="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-blue-500 hover:bg-blue-600 focus:ring-1 focus:ring-blue-900 text-white text-lg sm:text-xl duration-300"
-          aria-label="More Projects"
-        >
-          <Button :title="$t('More Projects')"/>
+          aria-label="More Projects">
+          <Button :title="$t('More Projects')" />
         </NuxtLink>
       </div>
     </div>
@@ -31,14 +29,24 @@ import { useI18n } from 'vue-i18n';
 import { useHead } from '#imports'
 import { useLocalePath } from '#i18n';
 
-const localePath = useLocalePath();
+const localePath=useLocalePath();
 
-const { t } = useI18n({
-    inheritLocale: true,
-    useScope: "global",
+const { t }=useI18n({
+  inheritLocale: true,
+  useScope: "global",
 });
 useHead({
-  title: () => `${t('Home')} - ${t('Mostefa Boudjema')}`
+  title: () => `${t('Home')} - ${t('Mostefa Boudjema')}`,
+  meta: [
+    {
+      name: 'description',
+      content: t('meta.home.description')
+    },
+    {
+      name: 'keywords',
+      content: t('meta.home.keywords')
+    }
+  ],
 })
 </script>
 
