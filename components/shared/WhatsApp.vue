2436521fa985111d85231393563fb4b583f16a70
mostfa <myreal17@gmail.com>
2025-08-10 01:32:34 +0100

    custom wats

--- components/shared/WhatsApp.vue
+++ components/shared/WhatsApp.vue
@@ -1,29 +1,43 @@
 <template>
-	<a id="whatsapp" class="mx-2" :href="`https://api.whatsapp.com/send?phone=${settings.whatsappNumber}`" data-aos="fade-up" data-aos-delay="200">
-		<img :height="70" :src="settings.whatsapp" class="my-4" alt="Icon" />
-	</a>
+    <a id="whatsapp" :href="`https://api.whatsapp.com/send?phone=${whatsappNumber}`" data-aos="fade-up"
+        data-aos-delay="200">
+        <img :src="whatsappImag" alt="Icon" />
+    </a>
 </template>
 
 <script setup>
 import settings from "~/configs";
+const whatsappNumber=settings.whatsappNumber; // Replace with your WhatsApp number
+const whatsappImag=settings.whatsapp; // Replace with your WhatsApp image path
 
 
 </script>
 
-<style scoped>
 
+<style scoped>
 #whatsapp {
     z-index: 99;
     position: fixed;
-    bottom: -5px;
-	width: 10vh;
-    left: 10px;
+    width: 10vh;
+    bottom: -20px;
+    left: 5px;
+    margin-right: .5rem !important;
+    margin-left: .5rem !important;
 }
+
+img {
+    width: 90px;
+    margin-top: 1.5rem !important;
+    margin-bottom: 1.5rem !important;
+}
+
 @media (max-width: 991px) {
     #whatsapp {
-        img {
-            width: 75px;
-        }
+        left: 10px;
+    }
+
+    img {
+        width: 95px;
     }
 }
 </style>
