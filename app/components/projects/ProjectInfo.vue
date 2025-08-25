6c8227156e9641be8c711b8dd3611195069e3507
mostfa <myreal17@gmail.com>
2025-08-25 04:41:17 +0100

    fix rtl

--- app/components/projects/ProjectInfo.vue
+++ app/components/projects/ProjectInfo.vue
@@ -1,59 +1,21 @@
-<script setup>
-import feather from 'feather-icons';
-import { onMounted, onUpdated, computed } from 'vue';
-import { useI18n } from 'vue-i18n';
-
-const props = defineProps({
-  projectInfo: {
-    type: Object,
-    required: true
-  },
-  smallImages: {
-    type: Array,
-    required: true
-  }
-});
-
-const { locale, t } = useI18n();
-
-const getFinalDetails = (info) => {
-  return locale.value === 'ar' ? (info.detailsAr || info.details) : info.details;
-};
-
-onMounted(() => {
-  feather.replace();
-});
-
-onUpdated(() => {
-  feather.replace();
-});
-</script>
-
 <template>
-  <div class="block sm:flex gap-0 sm:gap-10 mt-14">
+  <div class="block sm:flex gap-0 sm:gap-10 mt-14" :dir="locale === 'ar' ? 'rtl' : 'ltr'">
     <!-- Left section -->
-    <div class="w-full sm:w-1/3 text-left">
+    <div class="w-full sm:w-1/3 ">
       <!-- Client details -->
       <div class="mb-7">
         <p class="font-general-medium text-2xl text-secondary-dark dark:text-secondary-light mb-2">
           {{ t(projectInfo.clientHeading) }}
         </p>
         <ul class="leading-loose">
-          <li
-            v-for="(info, index) in projectInfo.companyInfos"
-            :key="index"
-            class="font-general-regular text-ternary-dark dark:text-ternary-light"
-          >
+          <li v-for="(info, index) in projectInfo.companyInfos" :key="index"
+            class="font-general-regular text-ternary-dark dark:text-ternary-light">
             <div v-if="info.details !== '#'">
               <span>{{ t(info.title) }}: </span>
 
               <template v-if="info.title === 'Website'">
-                <a
-                  :href="getFinalDetails(info)"
-                  class="hover:underline cursor-pointer"
-                  aria-label="Project Website and Phone"
-                  target="_blank"
-                >
+                <a :href="getFinalDetails(info)" class="hover:underline cursor-pointer"
+                  aria-label="Project Website and Phone" target="_blank">
                   {{ t(getFinalDetails(info).replace('project.info.', '')) }}
                 </a>
               </template>
@@ -81,14 +43,10 @@ onUpdated(() => {
         </p>
         <div class="flex space-x-2">
           <div v-for="(smallImg, index) in smallImages" :key="index">
-            <NuxtImg
-              :src="smallImg.src"
-              :alt="smallImg.alt"
-              :style="{
-                width: smallImg.width || '50px',
-                height: smallImg.height || '50px'
-              }"
-            />
+            <NuxtImg :src="smallImg.src" :alt="smallImg.alt" :style="{
+    width: smallImg.width || '50px',
+    height: smallImg.height || '50px'
+  }" />
           </div>
         </div>
       </div>
@@ -99,14 +57,9 @@ onUpdated(() => {
           {{ t(projectInfo.socialSharingsHeading) }}
         </p>
         <div class="flex items-center gap-3 mt-5">
-          <a
-            v-for="social in projectInfo.socialSharings"
-            :key="social.id"
-            :href="social.url"
-            target="__blank"
+          <a v-for="social in projectInfo.socialSharings" :key="social.id" :href="social.url" target="__blank"
             aria-label="Share Project"
-            class="bg-ternary-light dark:bg-ternary-dark text-gray-400 hover:text-primary-dark dark:hover:text-primary-light p-2 rounded-lg shadow-sm duration-500"
-          >
+            class="bg-ternary-light dark:bg-ternary-dark text-gray-400 hover:text-primary-dark dark:hover:text-primary-light p-2 rounded-lg shadow-sm duration-500">
             <i :data-feather="social.icon" class="w-4 lg:w-5 h-4 lg:h-5"></i>
           </a>
         </div>
@@ -114,17 +67,45 @@ onUpdated(() => {
     </div>
 
     <!-- Right section -->
-    <div class="w-full sm:w-2/3 text-left mt-10 sm:mt-0">
+    <div class="w-full sm:w-2/3 mt-10 sm:mt-0">
       <p class="font-general-medium text-primary-dark dark:text-primary-light text-2xl font-bold mb-7">
         {{ t(projectInfo.projectDetailsHeading) }}
       </p>
-      <p
-        v-for="projectDetail in projectInfo.projectDetails"
-        :key="projectDetail.id"
-        class="font-general-regular mb-5 text-lg text-ternary-dark dark:text-ternary-light"
-      >
+      <p v-for="projectDetail in projectInfo.projectDetails" :key="projectDetail.id"
+        class="font-general-regular mb-5 text-lg text-ternary-dark dark:text-ternary-light">
         {{ t(projectDetail.details) }}
       </p>
     </div>
   </div>
 </template>
+
+<script setup>
+import feather from 'feather-icons';
+import { onMounted, onUpdated, computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const props=defineProps({
+  projectInfo: {
+    type: Object,
+    required: true
+  },
+  smallImages: {
+    type: Array,
+    required: true
+  }
+});
+
+const { locale, t }=useI18n();
+
+const getFinalDetails=(info) => {
+  return locale.value==='ar'? (info.detailsAr||info.details):info.details;
+};
+
+onMounted(() => {
+  feather.replace();
+});
+
+onUpdated(() => {
+  feather.replace();
+});
+</script>
--- app/components/projects/ProjectRelatedProjects.vue
+++ app/components/projects/ProjectRelatedProjects.vue
@@ -1,59 +1,16 @@
-<script setup>
-import { useI18n } from "vue-i18n";
-import { computed } from 'vue';
-
-const props = defineProps({
-    relatedProject: {
-        type: Object,
-        required: true
-    }
-});
-
-const { t } = useI18n({
-    inheritLocale: true,
-    useScope: "global",
-});
-
-const randomItems = computed(() => {
-    // console.log('relatedProject:', props.relatedProject);
-    const arr = Array.isArray(props.relatedProject.relatedProjects)
-        ? props.relatedProject.relatedProjects
-        : [];
-    // Shuffle the items array using Fisher-Yates shuffle algorithm
-    const shuffled = arr.slice(0);
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    // Take the first 4 items from the shuffled array
-    return shuffled.slice(0, 4);
-});
-
-const setPageId = (id) => {
-    console.log('Page ID:', id);
-};
-</script>
-
 <template>
     <div class="border-primary-light dark:border-secondary-dark">
         <p
-            class="font-general-regular text-primary-dark dark:text-primary-light text-3xl font-bold mb-10 sm:mb-14 text-left"
-        >
-            {{ relatedProject.relatedProjectsHeading }}
+            class="font-general-regular text-primary-dark dark:text-primary-light text-3xl font-bold mb-10 sm:mb-14" :class="locale==='ar'?'text-right':'text-left'">
+            {{ $t(relatedProject.relatedProjectsHeading) }}
         </p>
 
-            <div class="grid grid-cols-1 sm:grid-cols-4 sm:gap-10 mt-12">
+        <div class="grid grid-cols-1 sm:grid-cols-4 sm:gap-10 mt-12">
             <!-- <div class="grid grid-cols-1 mt-6 sm:gap-10"> -->
             <div v-for="item in randomItems" :key="item.id">
-                <NuxtLink
-                    :to="item.link"
-                    @click="setPageId(item.id)"
-                >
-                    <NuxtImg
-                        :src="item.img"
-                        class="rounded-xl cursor-pointer h-70 w-96"
-                        :alt="item.title"
-                /></NuxtLink>
+                <NuxtLink :to="item.link" @click="setPageId(item.id)">
+                    <NuxtImg :src="item.img" class="rounded-xl cursor-pointer h-70 w-96" :alt="item.title" />
+                </NuxtLink>
                 <h3 class="dark:text-white text-center pt-2">
                     {{ item.title }}
                 </h3>
@@ -61,3 +18,39 @@ const setPageId = (id) => {
         </div>
     </div>
 </template>
+<script setup>
+import { useI18n } from "vue-i18n";
+import { computed } from 'vue';
+
+const props=defineProps({
+    relatedProject: {
+        type: Object,
+        required: true
+    }
+});
+
+const { locale }=useI18n();
+const { t }=useI18n({
+    inheritLocale: true,
+    useScope: "global",
+});
+
+const randomItems=computed(() => {
+    // console.log('relatedProject:', props.relatedProject);
+    const arr=Array.isArray(props.relatedProject.relatedProjects)
+        ? props.relatedProject.relatedProjects
+        :[];
+    // Shuffle the items array using Fisher-Yates shuffle algorithm
+    const shuffled=arr.slice(0);
+    for (let i=shuffled.length-1; i>0; i--) {
+        const j=Math.floor(Math.random()*(i+1));
+        [shuffled[i], shuffled[j]]=[shuffled[j], shuffled[i]];
+    }
+    // Take the first 4 items from the shuffled array
+    return shuffled.slice(0, 4);
+});
+
+const setPageId=(id) => {
+    console.log('Page ID:', id);
+};
+</script>
